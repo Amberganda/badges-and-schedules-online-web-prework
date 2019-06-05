0c6933a7cddb9e0d184924a95c94b09651eeca68
Amber Taranto <ambzzzz@gmail.com>
2019-06-05 03:26:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,8 +22,9 @@ end
 
 def printer(array)
     batch_badge_creator(array).each do |name|
-    puts name
-end
-assign_rooms(array).each do |assign_rooms|
-    puts assign_rooms
+        puts name
+    end
+    assign_rooms(array).each do |assign_rooms|
+        puts assign_rooms
+    end
 end
